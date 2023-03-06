a4633dfe8172c9f25a9965f1e960e1085fa18ef0
Christopher GUEST <chris.wilson@raytheon.co.uk>
2023-03-06 10:10:18 +0000

    finalised message

--- api/commute.ts
+++ api/commute.ts
@@ -32,6 +32,9 @@ type Metrolink = {
   LastUpdated: string;
 };
 
+const TIME_TO_WALK_EXSQ = 5;
+const TIME_TO_WALK_STPSQ = 10;
+
 export default async function trams(req: VercelRequest, resp: VercelResponse) {
   const { REACT_APP_TFGM_KEY } = process.env;
   if (REACT_APP_TFGM_KEY == null) {
@@ -76,30 +79,49 @@ export default async function trams(req: VercelRequest, resp: VercelResponse) {
       message: platform.MessageBoard,
     }));
 
-  const nextExchangeSqTram =
-    nearbyTrams.find(
-      (station) =>
-        station.stationName == "Exchange Square" &&
-        station.trams.some(
-          ({ destination, status }) =>
-            destination == "East Didsbury" && status == "Due"
-        )
-    ) ?? "No Tram Avaiable";
+  const nextExchangeSqArrivals = nearbyTrams.find(
+    (station) =>
+      station.stationName == "Exchange Square" &&
+      station.trams.some(
+        ({ destination, status }) =>
+          destination == "East Didsbury" && status == "Due"
+      )
+  );
 
-  const nextStPetersTram =
-    nearbyTrams.find(
-      (station) =>
-        station.stationName == "St Peter's Square" &&
-        station.trams.some(
-          ({ destination, status }) =>
-            destination == "Eccles via MediaCityUK" && status == "Due"
-        )
-    ) ?? "No Tram Avaiable";
+  const nextStPetersArrivals = nearbyTrams.find(
+    (station) =>
+      station.stationName == "St Peter's Square" &&
+      station.trams.some(
+        ({ destination, status }) =>
+          destination == "Eccles via MediaCityUK" && status == "Due"
+      )
+  );
 
-  const respBody = {
-    nextExchangeSqTram,
-    nextStPetersTram,
-  };
+  const timeToNextEastDidsburyTram = nextExchangeSqArrivals?.trams.find(
+    (tram) => tram.destination === "East Didsbury" && tram.status == "Due"
+  );
 
-  return resp.status(200).json(respBody);
+  const timeToNextMediaCityTram = nextStPetersArrivals?.trams.find(
+    (tram) =>
+      tram.destination == "Eccles via MediaCityUK" && tram.status == "Due"
+  );
+
+  let message = "";
+
+  if (timeToNextEastDidsburyTram && timeToNextMediaCityTram) {
+    const timeToWalkAndWait_EXSQ =
+      Number(timeToNextEastDidsburyTram.wait) + TIME_TO_WALK_EXSQ;
+    const timeToWalkAndWait_STPSQ =
+      Number(timeToNextMediaCityTram.wait) + TIME_TO_WALK_STPSQ;
+
+    if (timeToWalkAndWait_EXSQ < timeToWalkAndWait_STPSQ) {
+      message = "Please head to Exchange Sq";
+    } else {
+      message = "Please head to St Peter's Sqare 🤙";
+    }
+  } else {
+    message = "Unable to find trams, please check again 🤙";
+  }
+
+  return resp.status(200).json(message);
 }
